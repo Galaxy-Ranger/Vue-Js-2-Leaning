6841b321f644458204afabe579b403f4c8534a72
Galaxy-Ranger <surya.p.s780@gmail.com>
2025-03-08 17:28:32 +0530

    one little project i make on nested components before going to props in vue js

---
+++ #20_Nesting_Components_Examples/vuecli-examples/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer>
+    <p>{{ copyright }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      copyright: 'Copyright 2025 Vue Ninjas. All rights reserved.'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  footer {
+    background-color: #222;
+    padding: 6px;
+  }
+  p {
+    color: lightgreen;
+    text-align: center;
+  }
+</style>
---
+++ #20_Nesting_Components_Examples/vuecli-examples/src/components/Ninja.vue
@@ -0,0 +1,55 @@
+<template>
+  <main id="ninjas">
+    <ul>
+      <li v-for="ninja in ninjas" v-on:click="toggleNinja(ninja)">
+        <h2>{{ ninja.name }}</h2>
+        <h3 v-show="ninja.show">{{ ninja.speciality }}</h3>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script>
+export default {
+  data () {
+    return { ninjas: [
+      {name: 'Ryu', speciality: 'Vue Components', show: false},
+      {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+      {name: 'Hitoshi', speciality: 'Click Events', show: false},
+      {name: 'Tango', speciality: 'Conditionals', show: false},
+      {name: 'Kami', speciality: 'Webpack', show: false},
+      {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+    ]}
+  },
+  methods: {
+    toggleNinja(ninja) {
+      // console.log(this.ninjas.show)
+      ninja.show = !ninja.show
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #ninjas{
+    width: 100%;
+    max-width: 1200px;
+    margin: 40px auto;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+  ul{
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
+  }
+  li{
+    flex-grow: 1;
+    flex-basis: 300px;
+    text-align: center;
+    padding: 30px;
+    border: 1px solid #222;
+    margin: 10px;
+  }
+</style>
