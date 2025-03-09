cdffe2fc42555812b0c5a895f0f64fe61dc09244
Galaxy-Ranger <surya.p.s780@gmail.com>
2025-03-09 22:59:27 +0530

    One more mini project blog website

---
+++ #28_Creating_a_Blog/blog-website/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue'
+export default {
+  components: {
+    'add-blog': addBlog,
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap');
+body {
+  margin: 0;
+  font-family: "Nunito";
+}
+</style>
